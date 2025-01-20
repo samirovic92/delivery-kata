bfff6c86d60b8fe906eaa195ea95b3de9c7e389b
selidrissi <s.elidrissi1992@gmail.com>
2025-01-20 20:51:07 +0100

    Initialize project

---
+++ src/test/java/com/carrefour/deliverykata/DeliveryKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carrefour.deliverykata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryKataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
