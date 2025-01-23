fbc977445cfd68ca7cea83ffc4072de04a72759f
selidrissi <s.elidrissi1992@gmail.com>
2025-01-23 11:40:15 +0100

    Update security config

--- src/main/java/com/carrefour/deliverykata/config/SecurityConfig.java
+++ src/main/java/com/carrefour/deliverykata/config/SecurityConfig.java
@@ -5,12 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain testSecurityFilterChain(ServerHttpSecurity http) throws Exception {
-        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
-        return http.build();
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(
+                                "/swagger-ui.html",
+                                "/webjars/swagger-ui.html",
+                                "/webjars/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                        .anyExchange().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
     }
 }
