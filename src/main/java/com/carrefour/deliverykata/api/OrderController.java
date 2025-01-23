d963f8ab74a0114c743600aa5af741fa9f13e1b3
selidrissi <s.elidrissi1992@gmail.com>
2025-01-23 01:42:46 +0100

    [Tech] : add openapi library to document Rest Api

--- src/main/java/com/carrefour/deliverykata/api/OrderController.java
+++ src/main/java/com/carrefour/deliverykata/api/OrderController.java
@@ -2,6 +2,8 @@ package com.carrefour.deliverykata.api;
 
 import com.carrefour.deliverykata.api.Dto.CreateOrderRequest;
 import com.carrefour.deliverykata.domain.order.commands.CreatOrderCommand;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.http.HttpStatus;
@@ -13,11 +15,13 @@ import java.util.UUID;
 @AllArgsConstructor
 @RestController
 @RequestMapping("/orders")
+@Tag(name = "Order Management", description = "Endpoints for managing orders")
 public class OrderController {
     private final CommandGateway commandGateway;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "create order", description = "create order")
     public Mono<UUID> createOrder(@RequestBody CreateOrderRequest createOrderRequest) {
         var creatOrderCommand = new CreatOrderCommand(
                 UUID.randomUUID(),
--- src/main/java/com/carrefour/deliverykata/api/TimeSlotController.java
+++ src/main/java/com/carrefour/deliverykata/api/TimeSlotController.java
@@ -3,6 +3,8 @@ package com.carrefour.deliverykata.api;
 import com.carrefour.deliverykata.api.Dto.TimeSlotDto;
 import com.carrefour.deliverykata.domain.delivery.models.DeliveryMode;
 import com.carrefour.deliverykata.domain.delivery.TimeSlotService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/timeslots")
+@Tag(name = "TimeSlot Management", description = "Endpoints for TimeSlot orders")
 public class TimeSlotController {
 
     private final TimeSlotService timeSlotService;
@@ -22,6 +25,7 @@ public class TimeSlotController {
     }
 
     @GetMapping
+    @Operation(summary = "Available timeslot", description = "get all Available timeslot")
     public Flux<TimeSlotDto> getTimeSlots(@RequestParam DeliveryMode deliveryMode) {
         List<TimeSlotDto> timeSlots = timeSlotService.getAvailableTimeSlots(deliveryMode)
                 .stream()
